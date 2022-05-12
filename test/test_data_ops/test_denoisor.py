11e76bb6576c0993d70b723f29840457de1de569
tianhao <Tianhao_Fu@outlook.com>
2022-05-12 15:22:55 +0800

    add data_simulator&data_augmentor&data_denoisor test example

--- test/test_data_ops/test_denoisor.py
+++ test/test_data_ops/test_denoisor.py
@@ -1,1 +1,19 @@
+import torch
 import unitest
+
+from tiny3d.data.data_denoisor import lidar_denoisor
+
+class TestDataDenoisor(unittest.TestCase):
+
+    @classmethod
+    def setUpData(cls):
+        cls.points = torch.randn(2000,4)
+        cls.lidar_data = {"points": cls.points}
+        
+    # noinspection DuplicatedCode
+    def test_data_denoisor(self):
+        data_denoised = lidar_denoisor(cls.lidar_data, 'dmr')
+        assert data_denoised.shape == torch.size([2000,4])
+
+if __name__ == '__main__':
+    unittest.main()
