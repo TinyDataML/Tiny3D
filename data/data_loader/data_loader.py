3594a122ad13771d3721974848d9b0f8ed9e77b3
tianhao <TianhaoFu@outlook.com>
2022-05-29 15:53:59 +0800

    update

--- data/data_loader/data_loader.py
+++ data/data_loader/data_loader.py
@@ -3,7 +3,7 @@ import open3d as o3d
 import laspy
 import pcl
 
-def lidar_loader(lidar_data_path,lidar_dataset_path=None, method_name=None):
+def lidar_loader(lidar_data_path,lidar_dataset_path=None, type_name=None):
     """
         Use different lidar load methods to load lidar data.
         Args:
@@ -14,29 +14,19 @@ def lidar_loader(lidar_data_path,lidar_dataset_path=None, method_name=None):
         Reference:
             https://github.com/luost26/DMRDenoise
     """
-    if lidar_dataset_path =='None':
-        if method_name == 'numpy':
+    if not lidar_dataset_path:
+        if type_name == 'pcd.bin':
             points = np.fromfile(lidar_data_path, dtype=np.float32, count=-1).reshape([-1, 4])
 
-        if method_name == 'open3d':
-            # 文件类型是自动识别的，支持 .xyz .xyzn .xyzrgb .pts .ply .pcd类型的文件
-            pcd = o3d.io.read_point_cloud(lidar_data_path)
+        if type_name == '.xyz' or type_name == '.xyzn' or type_name == '.xyzrgb' or type_name == '.pts' or type_name == '.ply' or type_name == '.pcd':
+            #   .xyz .xyzn .xyzrgb .pts .ply .pcd
+            points = o3d.io.read_point_cloud(lidar_data_path)
 
-        if method_name == 'laspy':
-            data = laspy.read(lidar_data_path)
-            pcloud = np.vstack((data.X, data.Y, data.Z)).transpose()
+        # if type_name == 'laspy':
+            # data = laspy.read(lidar_data_path)
+            # points = np.vstack((data.X, data.Y, data.Z)).transpose()
 
-        if method_name == 'pcl':
-            pt = pcl.load(lidar_data_path)
+        # if method_name == 'pcl':
+            # points = pcl.load(lidar_data_path)
+            
     return points
-
-def read_pcd_file(pcd, test_pipeline, device, box_type_3d):
-    """Read data from pcd file and run test pipeline.
-
-    Args:
-        pcd (str): Pcd file path.
-        device (str): A string specifying device type.
-
-    Returns:
-        dict: meta information for the input pcd.
-    """
