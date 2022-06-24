d028b80039adbca08de53c8d369c8b920d93a64b
Crispig <crispy.g@qq.com>
2022-06-24 23:57:23 +0800

    init compressor.py

---
+++ model/model_compressor/compressor.py
@@ -0,0 +1,25 @@
+import torch.nn.utils.prune as prune
+import torch
+
+def torch_prune(model, prune_list, amount_list):
+
+    for n, module in model.named_modules():
+        for j, prune_item in enumerate(prune_list):
+            print(j)
+            print(prune_item)
+            if isinstance(module, prune_item):
+                prune.random_unstructured(module,name = 'weight', amount = amount_list[j])
+                prune.remove(module,'weight')
+
+def dynamic_quant(model):
+    model.cpu()
+    torch.quantization.quantize_dynamic(model, {torch.nn.Linear},  dtype=torch.qint8)
+    model.cuda()
+
+def static_quant(model, input_data):
+    model.qconfig = torch.quantization.get_default_qconfig('fbgemm')
+    model_prepared = torch.quantization.prepare(model)
+    model_prepared(*input_data)
+    model_prepared.cpu()
+    model_int8 = torch.quantization.convert(model_prepared, inplace=True)
+    torch_out = model_int8(*input_data)
