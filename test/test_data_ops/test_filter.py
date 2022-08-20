de18061954cc1b8a3ec9f0aa8a96cb8a1f836788
Daichao Chen <67040079+chendaichao@users.noreply.github.com>
2022-08-20 13:46:18 +0800

    Add code for unitest. Fix a bug related to dictionary copying.

--- test/test_data_ops/test_filter.py
+++ test/test_data_ops/test_filter.py
@@ -1,17 +1,19 @@
 import sys, os
 sys.path.append(".")
+import unittest
 
 from data.data_filter.data_filter import lidar_filter
 import numpy as np
 from matplotlib import pyplot as plt
 
-def draw_pointcloud(points, colors = None):
+def draw_pointcloud(points, colors = None, title = None):
 
     '''
         points: numpy array, of shape (n, 3)
               [(x, y, z), ...]
         colors: numpy array, of shape (n, 3)
                 [(r, g, b), ...]
+        title: str.
     '''
 
     extrinsic_mat = np.array(
@@ -38,24 +40,37 @@ def draw_pointcloud(points, colors = None):
     points = np.concatenate( [points[:, :2], np.ones((n, 1))], axis = 1 )
     points = (intrinsic_mat @ points.T).T
     plt.scatter(points[:, 0], points[:, 1], c = colors, marker = ".", s = 10)
+    if title:
+        plt.title(title)
     plt.show()
+
+
+class TestDownSample(unittest.TestCase):
+
+    def test_voxel_down_sample(self):
+        '''
+            Test voxel downsampling method.
+        '''
+        points = np.load("./test/test_data_ops/airplane.npy")
+        # add noise
+        # points += 0.05 * np.random.randn(*points.shape) * np.abs(points).max(axis = 0, keepdims = True)
+        print("Original number of points:", points.shape[0])
+        lidar_data = {"points": points}
+
+        draw_pointcloud(lidar_data["points"], title = "before")
+
+        filtered_lidar_data = lidar_filter( lidar_data, 
+                                            method = "voxel_down_sample", 
+                                            params = {"voxel_size" : 0.2,
+                                                    }
+                                        )
+        print(filtered_lidar_data["points"].shape)
+
+        draw_pointcloud(filtered_lidar_data["points"], title = "after")
+
+        assert filtered_lidar_data["points"].shape != lidar_data["points"].shape
 
 
 if __name__ == "__main__":
 
-    points = np.load("./test/test_data_ops/airplane.npy")
-    # add noise
-    # points += 0.05 * np.random.randn(*points.shape) * np.abs(points).max(axis = 0, keepdims = True)
-    print("Original number of points:", points.shape[0])
-    lidar_data = {"points": points}
-
-    draw_pointcloud(lidar_data["points"])
-
-    lidar_data = lidar_filter( lidar_data, 
-                               method = "voxel_down_sample", 
-                               params = {"voxel_size" : 0.2,
-                                         }
-                            )
-    print(lidar_data["points"].shape)
-
-    draw_pointcloud(lidar_data["points"])
+    unittest.main()
