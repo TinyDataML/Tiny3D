535ad2fe6e456ef4d6014b8e240da8b9577da097
Tianhao Fu <Tianhao_Fu@outlook.com>
2022-08-26 15:12:07 +0800

    Revert "Add code of unit test. Fix a bug in WLOP."
    This reverts commit 7b02576d23ec4f7ae7a5a932653d45d28e3cd7ba.

--- test/test_data_ops/test_filter.py
+++ test/test_data_ops/test_filter.py
@@ -2,14 +2,9 @@ import sys, os
 sys.path.append(".")
 import unittest
 
-import point_cloud_utils as pcu
 from data.data_filter.data_filter import lidar_filter
 import numpy as np
 from matplotlib import pyplot as plt
-
-def chamfer_distance(points1, points2):
-
-    return pcu.chamfer_distance(points1, points2)
 
 def draw_pointcloud(points, colors = None, title = None):
 
@@ -47,196 +42,35 @@ def draw_pointcloud(points, colors = None, title = None):
     plt.scatter(points[:, 0], points[:, 1], c = colors, marker = ".", s = 10)
     if title:
         plt.title(title)
-    #plt.show()
-    plt.pause(2)
+    plt.show()
 
-class TestDownSample:
 
-    def setUp(self) -> None:
-
-        plt.ion()
-        self.points = np.load("./test/test_data_ops/airplane.npy")
-        
-        print("Original number of points:",  self.points.shape[0])
-        draw_pointcloud(self.points, title = "original point cloud")
-    
-    def tearDown(self) -> None:
-
-        plt.ioff()
-        return super().tearDown()
+class TestDownSample(unittest.TestCase):
 
     def test_voxel_down_sample(self):
         '''
             Test voxel downsampling method.
         '''
-        lidar_data = {"points": self.points}
+        points = np.load("./test/test_data_ops/airplane.npy")
+        # add noise
+        # points += 0.05 * np.random.randn(*points.shape) * np.abs(points).max(axis = 0, keepdims = True)
+        print("Original number of points:", points.shape[0])
+        lidar_data = {"points": points}
+
+        draw_pointcloud(lidar_data["points"], title = "before")
+
         filtered_lidar_data = lidar_filter( lidar_data, 
                                             method = "voxel_down_sample", 
                                             params = {"voxel_size" : 0.2,
                                                     }
                                         )
-        draw_pointcloud(filtered_lidar_data["points"], title = "voxel downsample")
-        print("Number of points (voxel downsample): ", filtered_lidar_data["points"].shape[0])
-        assert filtered_lidar_data["points"].shape[0] < lidar_data["points"].shape[0]
-        assert filtered_lidar_data["points"].shape[1] >= 3
+        print(filtered_lidar_data["points"].shape)
 
-    def test_random_sample(self):
-        '''
-            Test random downsampling method.
-        '''
+        draw_pointcloud(filtered_lidar_data["points"], title = "after")
 
-        lidar_data = {"points": self.points}
-        filtered_lidar_data = lidar_filter( lidar_data, 
-                                            method = "random_sample", 
-                                            params = {"n_points" : 256,
-                                                    }
-                                        )
-        draw_pointcloud(filtered_lidar_data["points"], title = "random downsample")
-        print("Number of points (random downsample): ", filtered_lidar_data["points"].shape[0])
-        assert filtered_lidar_data["points"].shape[0] < lidar_data["points"].shape[0]
-        assert filtered_lidar_data["points"].shape[1] >= 3
+        assert filtered_lidar_data["points"].shape != lidar_data["points"].shape
 
-    def test_uniform_down_sample(self):
-        '''
-            Test uniform downsampling method.
-        '''
-
-        lidar_data = {"points": self.points}
-        filtered_lidar_data = lidar_filter( lidar_data, 
-                                            method = "uniform_down_sample", 
-                                            params = {"voxel_size" : 0.2,
-                                                    }
-                                        )
-        draw_pointcloud(filtered_lidar_data["points"], title = "uniform downsample")
-        print("Number of points (uniform downsample): ", filtered_lidar_data["points"].shape[0])
-        assert filtered_lidar_data["points"].shape[0] < lidar_data["points"].shape[0]
-        assert filtered_lidar_data["points"].shape[1] >= 3
-
-    def test_farthest_point_sample(self):
-        '''
-            Test farthest point downsampling method.
-        '''
-
-        lidar_data = {"points": self.points}
-        filtered_lidar_data = lidar_filter( lidar_data, 
-                                            method = "farthest_point_sample", 
-                                            params = {"n_points" : 512,
-                                                    }
-                                        )
-        draw_pointcloud(filtered_lidar_data["points"], title = "farthest point sample")
-        print("Number of points (farthest point sample): ", filtered_lidar_data["points"].shape[0])
-        assert filtered_lidar_data["points"].shape[0] < lidar_data["points"].shape[0]
-        assert filtered_lidar_data["points"].shape[1] >= 3
-
-class TestOutlierRemoval():
-
-    def setUp(self) -> None:
-
-        plt.ion()
-        self.points = np.load("./test/test_data_ops/airplane.npy")
-        num_points, num_channels = self.points.shape
-
-        # add outliers
-        self.points = np.concatenate(
-            (self.points, 
-             np.random.randn(20, num_channels) * self.points.std(axis = 0, keepdims = True)
-            ),
-            axis = 0,
-        )
-        
-        print("Original number of points:",  num_points + 20)
-        draw_pointcloud(self.points, title = "original point cloud")
-    
-    def tearDown(self) -> None:
-
-        plt.ioff()
-        return super().tearDown()
-
-    def test_remove_radius_outlier(self):
-        '''
-            Test radius outlier removal method.
-        '''
-        lidar_data = {"points": self.points}
-        filtered_lidar_data = lidar_filter( lidar_data, 
-                                            method = "remove_radius_outlier", 
-                                            params = {"nb_points" : 10,
-                                                      "radius" : 0.3,
-                                                    }
-                                        )
-        draw_pointcloud(filtered_lidar_data["points"], title = "remove radius outlier")
-        print("Number of points (remove radius outlier): ", filtered_lidar_data["points"].shape[0])
-        assert filtered_lidar_data["points"].shape[0] <= lidar_data["points"].shape[0]
-        assert filtered_lidar_data["points"].shape[1] >= 3
-
-    def test_remove_statistical_outlier(self):
-        '''
-            Test statistical outlier removal method.
-        '''
-        lidar_data = {"points": self.points}
-        filtered_lidar_data = lidar_filter( lidar_data, 
-                                            method = "remove_statistical_outlier", 
-                                            params = {"nb_neighbors" : 20,
-                                                      "std_ratio" : 2.0,
-                                                    }
-                                        )
-        draw_pointcloud(filtered_lidar_data["points"], title = "remove statistical outlier")
-        print("Number of points (remove statistical outlier): ", filtered_lidar_data["points"].shape[0])
-        assert filtered_lidar_data["points"].shape[0] <= lidar_data["points"].shape[0]
-        assert filtered_lidar_data["points"].shape[1] >= 3
-
-class TestSmooth(unittest.TestCase):
-
-    def setUp(self) -> None:
-
-        plt.ion()
-        self.original_points = np.load("./test/test_data_ops/airplane.npy")
-        
-        # add noise
-        self.points = self.original_points.copy()
-        self.points += 0.05 * np.random.randn(*self.points.shape) * np.std(self.points, axis = 0, keepdims = True)
-        
-        print("CD (noisy):",  chamfer_distance(self.points, self.original_points))
-        draw_pointcloud(self.points, title = "original point cloud")
-    
-    def tearDown(self) -> None:
-
-        plt.ioff()
-        return super().tearDown()
-
-    # def test_bilateral_filter(self):
-    #     '''
-    #         Test bilateral filtering method.
-    #     '''
-    #     lidar_data = {"points": self.points}
-    #     filtered_lidar_data = lidar_filter( lidar_data, 
-    #                                         method = "bilateral_filter", 
-    #                                         params = {"radius" : 0.5,
-    #                                                   "sigma_d" : 1.0,
-    #                                                   "sigma_n" : 1.0,
-    #                                                   "knn" : 20,
-    #                                                 }
-    #                                     )
-    #     draw_pointcloud(filtered_lidar_data["points"], title = "bilateral filter")
-    #     print("CD (bilateral filter):",  chamfer_distance(filtered_lidar_data["points"], self.original_points))
-    #     assert filtered_lidar_data["points"].shape[0] <= lidar_data["points"].shape[0]
-    #     assert filtered_lidar_data["points"].shape[1] >= 3
-
-    def test_weighted_local_optimal_projection(self):
-        '''
-            Test weighted local optimal projection method.
-        '''
-        lidar_data = {"points": self.points}
-        filtered_lidar_data = lidar_filter( lidar_data, 
-                                            method = "weighted_local_optimal_projection", 
-                                            params = {"n_points" : 1024,
-                                                      "num_iter" : 100,
-                                                    }
-                                        )
-        draw_pointcloud(filtered_lidar_data["points"], title = "wlop")
-        print("CD (wlop):",  chamfer_distance(filtered_lidar_data["points"], self.original_points))
-        assert filtered_lidar_data["points"].shape[0] <= lidar_data["points"].shape[0]
-        assert filtered_lidar_data["points"].shape[1] >= 3
 
 if __name__ == "__main__":
-        
+
     unittest.main()
